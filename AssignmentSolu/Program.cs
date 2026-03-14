86ae9809107e03532ead8e0f7cd5258864f7cdb1
Youss <Youss@ENG_JOE>
2026-03-14 17:24:42 +0200

    Add project files.

---
+++ AssignmentSolu/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AssignmentSolu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
